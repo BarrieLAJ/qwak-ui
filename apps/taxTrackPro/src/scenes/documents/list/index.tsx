566245db7566c9dba03610512133bc98e9d75132
Alhaji Abubakarr Barrie <alhajiab.barrie@gmail.com>
2023-07-03 03:36:35 +0000

    Updates... UI Tax laws and Documnets

--- apps/taxTrackPro/src/scenes/documents/list/index.tsx
+++ apps/taxTrackPro/src/scenes/documents/list/index.tsx
@@ -1,13 +1,23 @@
 import React from "react";
-import { Box, Button } from "ui";
+import { Box, Button, Divider } from "ui";
+import { TaxLawCard } from "~/components/common/TaxLawCard";
 
 const DocumentList = () => {
   return (
-    <Box className="flex h-full w-full flex-col items-center justify-center gap-2">
-      <NoDocuments />
-      <Button intent="primary"  variant="filled" size="md">
-        Upload document
-      </Button>
+    <Box className="flex h-full w-full flex-col items-center justify-center gap-16">
+      {false && <NoDocuments />}
+      <Box className="flex h-max max-w-[85%] flex-wrap gap-16 p-10">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 3, 4, 5].map((v, i) => {
+          return <TaxLawCard />;
+        })}
+      </Box>
+      <Box className="w-full">
+        <Divider className="my-10 w-4/5 bg-slate-300" />
+        <Button className="mx-auto" intent="primary" variant="filled" size="lg">
+          Upload document
+        </Button>
+        <Divider className="my-10 w-4/5 bg-slate-300" />
+      </Box>
     </Box>
   );
 };
@@ -16,10 +26,6 @@ export default DocumentList;
 
 export const SingleDocumentsView = () => {
   return <Box className="flex flex-col gap-4 p-4 shadow-sm"></Box>;
-};
-
-export const SingleDocument = () => {
-  return <Box className=""></Box>;
 };
 
 export function NoDocuments() {
